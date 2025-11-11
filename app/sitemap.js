9ce9dc41a2ccb416668e2123eebb8cad8a0c35f0
gridialhub <dmb.vzla@gmail.com>
2025-11-11 15:21:31 -0800

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,1 +1,12 @@
+export default function sitemap() {
+  const base = "https://gridialhub.com";
+  return [
+    { url: base, lastModified: new Date() },
+    { url: `${base}/sorteos`, lastModified: new Date() },
+    { url: `${base}/articulos`, lastModified: new Date() },
+    { url: `${base}/resultados`, lastModified: new Date() },
+    { url: `${base}/bases`, lastModified: new Date() },
+    { url: `${base}/contacto`, lastModified: new Date() },
+  ];
+}
 
