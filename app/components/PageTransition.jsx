7062a689c8b33624968851a8d5ff73e4774424d3
gridialhub <dmb.vzla@gmail.com>
2025-11-11 14:24:03 -0800

    Update PageTransition.jsx

--- app/components/PageTransition.jsx
+++ app/components/PageTransition.jsx
@@ -1,1 +1,13 @@
+"use client";
+import { usePathname } from "next/navigation";
+
+export default function PageTransition({ children }) {
+  const pathname = usePathname();
+  // Esto remonta la animación cada vez que cambias de página
+  return (
+    <div key={pathname} className="page-fade">
+      {children}
+    </div>
+  );
+}
 
